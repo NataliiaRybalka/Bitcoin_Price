7fb41da6dbfca43fc51f9ce6af9dc2b4447e2503
Nataliia Rybalka <natalirybalka21@gmail.com>
2021-07-23 17:11:19 +0300

    sort

--- client/src/components/PriceTable.js
+++ client/src/components/PriceTable.js
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import './PriceTable.css';
 
 export default function PriceTable(props) {
-  const {price} = props;
+  const [ignored, updateState] = useState();
+  const forceUpdate = useCallback(() => updateState({}), []);
+
+  const { price } = props;
+  
+  const sortPrice = (param, direction) => {
+    if (param === 'date') {
+      if (direction === 'up') {
+        price.sort((a, b) => {
+          if (a.date > b.date) { 
+            return -1; 
+          }
+          if (a.date < b.date) { 
+            return 1; 
+          }
+          return 0;
+        });
+      } else if (direction === 'down') {
+        price.sort((a, b) => {
+          if (a.date < b.date) { 
+            return -1; 
+          }
+          if (a.date > b.date) { 
+            return 1; 
+          }
+          return 0;
+        });
+      }
+    } else if (param === 'price') {
+      if (direction === 'up') {
+        price.sort((a, b) => b.price - a.price);
+      } else if (direction === 'down') {
+        price.sort((a, b) => a.price - b.price);
+      }
+    }
+
+    forceUpdate();
+  }
 
   return (
     <div>
       <table>
         <thead>
           <tr>
-            <td>Дата</td>
-            <td>Цена</td>
+            <td>
+              <span className='up' onClick={()=>sortPrice('date', 'down')}>&uarr;</span>
+              Дата
+              <span className='down' onClick={()=>sortPrice('date', 'up')}>&darr;</span>
+            </td>
+            <td>
+              <span className='up' onClick={()=>sortPrice('price', 'down')}>&uarr;</span>
+              Цена
+              <span className='down' onClick={()=>sortPrice('price', 'up')}>&darr;</span>
+            </td>
           </tr>
         </thead>
         <tbody>
